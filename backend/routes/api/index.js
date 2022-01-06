cd014ca8ec20736ffa6e9385969a748463696d69
will <w.duffy@outlook.com>
2022-01-06 15:08:23 -0600

    created frontend to request csrf form backend

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -6,8 +6,5 @@ router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
-router.post('/test', (req, res) => {
-  res.json({ requestBody: req.body });
-});
 
 module.exports = router;
