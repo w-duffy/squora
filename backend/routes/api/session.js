a2b1e571755f4efe50d7a7523a9b94c272888769
will <w.duffy@outlook.com>
2022-01-06 12:30:14 -0600

    created log-in validation

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
 const router = express.Router();
 
+const validateLogin = [
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email or username."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors,
+];
+
 //log-in
 router.post(
   "/",
+  validateLogin,
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
@@ -32,10 +46,9 @@ router.post(
 
 // Log out
 router.delete("/", (_req, res) => {
-    res.clearCookie("token");
-    return res.json({ message: "success" });
-  });
-
+  res.clearCookie("token");
+  return res.json({ message: "success" });
+});
 
 // Restore session user
 router.get("/", restoreUser, (req, res) => {
@@ -46,6 +59,5 @@ router.get("/", restoreUser, (req, res) => {
     });
   } else return res.json({});
 });
-
 
 module.exports = router;
