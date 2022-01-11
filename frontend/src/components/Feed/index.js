ecbf516494333cfc750d478773ccdab4e476109c
will <w.duffy@outlook.com>
2022-01-10 23:54:58 -0600

    added styles to the main splash page

--- frontend/src/components/Feed/index.js
+++ frontend/src/components/Feed/index.js
@@ -5,69 +5,83 @@ import { getUsers } from "../../store/user";
 import DeleteFormModal from "./DeleteFormModal";
 import EditFormModal from "./EditFormModal";
 import { useHistory } from "react-router-dom";
-import QuestionModal from '../Questions/QuestionModal'
-import './Feed.css'
-
-
+import QuestionModal from "../Questions/QuestionModal";
+import "./Feed.css";
 
 function Feed() {
   const sessionUser = useSelector((state) => state.session.user);
   const dispatch = useDispatch();
   const [deleteClick, setDeleteClick] = useState(null);
   const [loaded, setLoaded] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
+
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = () => {
+      setShowMenu(false);
+    };
+
+    document.addEventListener("click", closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
 
   const history = useHistory();
 
+  const questions = useSelector((state) => state.questions.questions);
+  const users = useSelector((state) => state.users.users);
 
-  const questions = useSelector(state => state.questions.questions);
-  const users = useSelector(state => state.users.users);
+  const [stateChange, setStateChange] = useState("no");
 
-
-  const [stateChange, setStateChange] = useState("no")
-
-
-
-  useEffect( async () => {
-    await dispatch(getUsers())
-     await dispatch(getQuestions());
-     await setLoaded(true)
+  useEffect(async () => {
+    await dispatch(getUsers());
+    await dispatch(getQuestions());
+    await setLoaded(true);
   }, []);
-
 
   if (!questions) {
     return null;
   }
 
-if (loaded){
-  return (
-
-    <div>
-      <div className="ask-question">
-        <QuestionModal />
-      </div>
-      {questions.map((question) => (
-        <div className="ask-question" key={question.id}>
-          <div className="feed-content">
-            {users.map((user) => (user.id === question.ownerId?
-              <div className="feed-content" key={Math.random()}>
-              <p className="username">{user.username} asked: </p>
-              </div>
-              : <div key={Math.random()}></div>
-              ))}
-            <div>
-              <div className="feed-content">
-            <p className="p-description">{question.description}</p>
-              </div>
-              </div>
-            <DeleteFormModal question={question} />
-            <EditFormModal question={question} />
-            </div>
+  if (loaded) {
+    return (
+      <div>
+        <div className="ask-question">
+          <QuestionModal />
         </div>
-      ))}
-    </div>
-
-)
-} else return <></>
+        {questions.map((question) => (
+          <div className="ask-question" key={question.id}>
+            <div className="feed-content">
+              {users.map((user) =>
+                user.id === question.ownerId ? (
+                  <div className="feed-content" key={Math.random()}>
+                    <p className="username">{user.username} asked: </p>
+                  </div>
+                ) : (
+                  <div key={Math.random()}></div>
+                )
+              )}
+              <div>
+                <div className="feed-content">
+                  <p className="p-description">{question.description}</p>
+                </div>
+              </div>
+              <div className="edit-delete">
+                <DeleteFormModal question={question} />
+                <EditFormModal question={question} />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  } else return <></>;
 }
 
 export default Feed;
