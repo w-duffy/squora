9a47063b2b953df217b4b6b5b58263a851b1ce0f
will <w.duffy@outlook.com>
2022-01-11 14:31:43 -0600

    updated splash page styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,11 +29,47 @@ function App() {
           <div className="Title">
             <h1>Squora</h1>
             <div>
-            <p>A place to share investing knowledge and better understand the markets</p>
+              <p>
+                A place to share investing knowledge and better understand the
+                markets
+              </p>
             </div>
           </div>
           <div className="Links">
-            test
+            <a href="https://github.com/w-duffy/squora" target="_blank">
+              <img
+                src={"https://cdn-icons-png.flaticon.com/512/25/25231.png"}
+                alt="github"
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/will-duffy-a46a7a8a/"
+              target="_blank"
+            >
+              <img
+                src={
+                  "https://cdn3.iconfinder.com/data/icons/free-social-icons/67/linkedin_circle_black-512.png"
+                }
+                alt="LinkedIn"
+              />
+            </a>
+            <a
+              href="https://www.appacademy.io/enterprise/hiring?utm_medium=ppc&utm_source=google&utm_campaign=14640069351&gclid=Cj0KCQiA8vSOBhCkARIsAGdp6RTVy85MXjwkVGN3f0-ripxBOp9676sXnVJ-uqyIB-7mNQSywGsWiLcaAijhEALw_wcB"
+              target="_blank"
+            >
+              <img className="aa"
+                src={
+                  "https://assets-global.website-files.com/5dcc7f8c449e597ed83356b8/5dcf6294afa6ed60b2324b60_logo-full-white-1000.png"
+                }
+                alt="App Academy"
+              />
+            </a>
+            <a href="https://memehub-medium-clone.herokuapp.com/" target="_blank">
+              <img className="memehub"
+                src={"https://i.ibb.co/RBXZMcT/Screenshot-2022-01-11-142127.png"}
+                alt="MemeHub"
+              />
+            </a>
           </div>
           <div className="Buttons">
             <Navigation isLoaded={isLoaded} />
@@ -45,9 +81,7 @@ function App() {
               </Switch>
             )}
           </div>
-          <div className="Description">
-            test
-          </div>
+          <div className="Description">test</div>
         </div>
       </>
     );
